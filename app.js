e91678097b0ad1389f3b5e93cdf4b7952f0d3258
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2021-11-22 22:22:18 +0100

    last modif

---
+++ app.js
@@ -0,0 +1,38 @@
+//  :::::::::::::::::::::::::::::::: DOM ::::::::::::::::::::::::::::::
+const touches =[...document.querySelectorAll('.button')];
+const listekeycode = touches.map(touche => touche.dataset.key); //acceder a data-key grace a dataset  
+const ecran = document.querySelector('.ecran');
+
+document.addEventListener('keydown', (e) =>{
+    const valeur = e.keyCode.toString();
+    calculer(valeur)
+
+})
+
+document.addEventListener('click', (e) =>{
+    const valeur = e.target.dataset.key;
+    calculer(valeur)
+
+})
+
+const calculer = (valeur) => {
+    if (listekeycode.includes(valeur)){
+        switch(valeur){
+            case '8':
+                ecran.textContent ="";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul;
+                break;
+            default:
+            const indexkeycode = listekeycode.indexOf(valeur);
+            const touche = touches[indexkeycode];
+            ecran.textContent += touche.innerHTML;
+        }
+    }
+}
+
+window.addEventListener('error', (e) => {
+    alert('Ach katkharbe9 ??: ' + e.message)
+})
